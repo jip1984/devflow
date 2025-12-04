ec6978e6dd2b64ec3c4a116069d1f45bee83962e
Jon <jonprosser84@gmail.com>
2025-12-04 17:56:37 +0000

    add assets and meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Dev Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
+};
 
 const page = () => {
   return (
     <div>
-      <h1 className='text-3xl'>Welcome to the ultimate next js course</h1>
+      <h1 className='text-3xl text-light-500'>Welcome to the ultimate next js course</h1>
     </div>
   )
 }
