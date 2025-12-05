35927bcd56f73b37d075a9e3f80148b3b3c54c3a
Jon <jonprosser84@gmail.com>
2025-12-05 17:35:11 +0000

    set up light and dark theme, shadcn and navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import React from "react";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import NavBar from "@/components/ui/navigation/navbar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -26,11 +28,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <NavBar />
+          {children}
+          </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 
-
 export const metadata: Metadata = {
   title: "Dev Overflow",
   description:
